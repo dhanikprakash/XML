ea235f33d8493079248a3a68599829e8b98ebea2
dhanikprakash <dhanikprakash@gmail.com>
2019-08-22 22:53:46 +0100

    Initial cleaning

--- XML/AssetSearch.cs
+++ XML/AssetSearch.cs
@@ -12,11 +12,7 @@ namespace XML
 
         public string SearchAsset(string assetName, string searchScope)
         {
-            //Xml-improvements #1
-            //Xml-improvements #2
-            //Xml-improvements #3
-            //Xml-improvements #4
-            //Xml-improvements #5
+           
             var search = new SearchModel();
             var searchBlock = new SearchBlock();
             var searchParams = new SearchParams();
@@ -28,21 +24,6 @@ namespace XML
             search.searchBlock.Add(searchBlock);
 
             var sResult = Serializer.Serialize<SearchModel>(search);
-
-            var j = 1;
-            var k = 2;
-            var x = 1;
-            var z = 0;
-            // var model = new SearchModelV2(assetName, searchScope);
-            //var xmlNode = new XElement("Search",
-            //                     new XAttribute("SearchScope", model.SearchScope),
-            //                     new XAttribute("xmls", "urn:OPUSSearch"),
-            //                     new XElement("SearchBlock",
-            //                     new XElement("Name",
-            //                     new XAttribute("Value", model.AssetName),
-            //                     new XAttribute("Operater", model.Operation)))).ToString();
-
-
             return sResult;
         }
 
